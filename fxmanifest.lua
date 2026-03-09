2c0fa897a06f579f4cf9945ac7439361c0ae1468
Taylor <taylorwhitewood2022@gmail.com>
2026-03-09 21:24:04 +0100

    Add initial implementation of car bomb feature with configuration and server handling

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "wz-carbomb"
+description "Adds a useable car bomb to your server for players to use in RP"
+author "Whitezom"
+version "1.0.0"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
---
+++ shared/main.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+-- General Settings
+Config.PhoneResource = "npwd" -- Suitable resources include "npwd" and "lb_phone".
+Config.CarBombItem = "car_bomb"
+
+-- Planting Mechanics
+Config.PlantTime = 5000 -- ms (How long the progress bar lasts)
+Config.PlantAnimation = {
+    dict = "anim@amb@business@weed@weed_inspecting_lo_med_hi@",
+    anim = "weed_stand_check_low_idle01_inspector"
+}
+
+
+Config.ExcludeVehicles = {
+    Enabled = true,
+    List = {
+        ["police"] = true,
+        ["police2"] = true
+    }
+}
+
+
+
